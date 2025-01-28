39f36b23545f3357e6f2e7349fea61ea1b7afb31
Tom Maurer <tmaurer@nait.ca>
2025-01-28 09:34:04 -0700

    pt 2

--- Week4/stateful_dog_application/lib/main.dart
+++ Week4/stateful_dog_application/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +30,8 @@ class MyApp extends StatelessWidget {
 class RandomDogImage extends StatefulWidget {
   const RandomDogImage({super.key});
 
+  final String endpoint = 'https://dog.ceo/api/breeds/image/random';
+
   @override
   State<RandomDogImage> createState() => _RandomDogImageState();
 }
@@ -36,18 +40,46 @@ class _RandomDogImageState extends State<RandomDogImage> {
   // Create state variables
   String dogImageUrl = 'https://images.dog.ceo/breeds/terrier-welsh/lucy.jpg';
 
-  // TODO: Create a helper function getRandomDogURL
+  // Create a helper function getRandomDogURL
+  Future<String> getRandomDogURL() async {
+    var endpointURI = Uri.parse(widget.endpoint);
+    var response = await http.get(endpointURI);
+    return jsonDecode(response.body)['message'];
+  }
 
-  // TODO: Override the initState to fetch a new dog on load
+  // Override the initState to fetch a new dog on load
+  @override
+  void initState() {
+    super.initState();
+
+    getRandomDogURL().then((url) {
+      setState(() {
+        dogImageUrl = url;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // Display the dog image
-
-    // TODO: Display a button that refreshes the dog image
+    // Display a button that refreshes the dog image
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.network(dogImageUrl),
+        Image.network(
+          dogImageUrl,
+          height: 250,
+        ),
+        TextButton(
+          onPressed: () {
+            getRandomDogURL().then((url) {
+              setState(() {
+                dogImageUrl = url;
+              });
+            });
+          },
+          child: Text('Refresh Dog Image'),
+        )
       ],
     );
   }
