6bb8fbd640e74fb2752748de39ec08455eaaceb0
Tom Maurer <tmaurer@nait.ca>
2025-03-05 09:38:09 -0700

    riverpod

---
+++ Week8/riverpod_example/lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class User extends ChangeNotifier {
+  User(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  String? _firstName;
+  String? _lastName;
+
+  String? get firstName => _firstName;
+  String? get lastName => _lastName;
+
+  set firstName(newFirstName) {
+    _firstName = newFirstName;
+    notifyListeners();
+  }
+
+  set lastName(newLastName) {
+    _lastName = newLastName;
+    notifyListeners();
+  }
+}
+
+// Finally, we are using ChangeNotifierProvider to allow the UI to interact
+// with our User class. Everywhere we import this file, the provider will be
+// available
+final userProvider = ChangeNotifierProvider<User>((ref) {
+  return User('James', 'Potter'); // Initial default user
+});
